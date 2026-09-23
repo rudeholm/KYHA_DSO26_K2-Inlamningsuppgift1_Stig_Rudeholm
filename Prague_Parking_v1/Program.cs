26d63fd288b44a8dc81b9cdd2b04232797d55fd5
Grakkam <grakkam@gmail.com>
2026-09-23 22:29:58 +0200

    An encoded car license plate can be decoded to display it properly

--- Prague_Parking_v1/Program.cs
+++ Prague_Parking_v1/Program.cs
@@ -15,6 +15,18 @@ static void RunTests()
         EncodeCarLicensePlate("  foo 666 ").Equals("CAR#FOO_666")
         );
 
+    Console.WriteLine(
+        "An encoded car license plate can be decoded to display it properly: {0}",
+        DecodeLicensePlate("CAR#FOO_666").Equals("FOO 666")
+        );
+
+}
+
+static object DecodeLicensePlate(string v)
+{
+    v = v.Split('#')[1];
+    v = v.Replace('_', ' ');
+    return v;
 }
 
 static string EncodeCarLicensePlate(string licensePlate)
