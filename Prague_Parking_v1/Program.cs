18f29de806d04f79260fb9ae2c2925ca6017df2b
Grakkam <grakkam@gmail.com>
2026-09-24 00:29:02 +0200

    Added some color to the test results

--- Prague_Parking_v1/Program.cs
+++ Prague_Parking_v1/Program.cs
@@ -18,46 +18,63 @@ static void RunTests()
     // Tests
     Console.WriteLine(
         "A car license plate can be encoded for storage in the database: {0}",
-        EncodeCarLicensePlate("  foo 666 ").Equals("CAR#FOO_666")
+        FormatResult(EncodeCarLicensePlate("  foo 666 ").Equals("CAR#FOO_666"))
         );
 
     Console.WriteLine(
         "A motorcycle license plate can be encoded for storage in the database: {0}",
-        EncodeMotorcycleLicensePlate("M-bar 999 ").Equals("MC#M-BAR_999")
+        FormatResult(EncodeMotorcycleLicensePlate("M-bar 999 ").Equals("MC#M-BAR_999"))
         );
 
     Console.WriteLine(
         "An encoded license plate can be decoded to display it properly: {0}",
-        DecodeLicensePlate("CAR#FOO_666").Equals("FOO 666")
-        && DecodeLicensePlate("MC#BAZ_M-222").Equals("BAZ M-222")
+        FormatResult(DecodeLicensePlate("CAR#FOO_666").Equals("FOO 666")
+        && DecodeLicensePlate("MC#BAZ_M-222").Equals("BAZ M-222"))
         );
 
     testSpaceId = 2;
     Console.WriteLine(
         "You can check if a parking space is empty: {0}",
-        ParkingSpaceIsEmpty(testSpaceId, testGarage) == true
+        FormatResult(ParkingSpaceIsEmpty(testSpaceId, testGarage) == true)
         );
 
     testSpaceId = 1;
     AssignParkingSpaceToVehicle(testSpaceId, testCarLicensePlate, testGarage);
     Console.WriteLine(
         "You can check if a parking space is NOT empty: {0}",
-        ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true
+        FormatResult(ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true)
         );
 
     testSpaceId = 2;
     Console.WriteLine(
         "You can assign an empty space to a vehicle: {0}",
-        AssignParkingSpaceToVehicle(testSpaceId, testCarLicensePlate, testGarage) == true
+        FormatResult(AssignParkingSpaceToVehicle(testSpaceId, testCarLicensePlate, testGarage) == true)
         );
 
     testSpaceId = 2;
     AssignParkingSpaceToVehicle(testSpaceId, testCarLicensePlate, testGarage);
     Console.WriteLine(
         "After assigning an empty space to a vehicle, the space is not empty: {0}",
-        ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true
+        FormatResult(ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true)
         );
 
+
+
+    static bool FormatResult(bool result)
+    {
+        if (result == true)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+
+        return result;
+    }
+
+    Console.ResetColor();
 }
 
 /*****************************************************************************/
