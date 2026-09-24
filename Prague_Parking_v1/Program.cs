6d3a7cf6cef087021d1b18a44c11b2767b3988c7
Grakkam <grakkam@gmail.com>
2026-09-24 12:57:52 +0200

    After clearing a non-empty space, the space is empty again

--- Prague_Parking_v1/Program.cs
+++ Prague_Parking_v1/Program.cs
@@ -18,63 +18,64 @@ static void RunTests()
     // Tests
     Console.WriteLine(
         "A car license plate can be encoded for storage in the database: {0}",
-        FormatResult(EncodeCarLicensePlate("  foo 666 ").Equals("CAR#FOO_666"))
+        FormatTestResult(EncodeCarLicensePlate("  foo 666 ").Equals("CAR#FOO_666"))
         );
 
     Console.WriteLine(
         "A motorcycle license plate can be encoded for storage in the database: {0}",
-        FormatResult(EncodeMotorcycleLicensePlate("M-bar 999 ").Equals("MC#M-BAR_999"))
+        FormatTestResult(EncodeMotorcycleLicensePlate("M-bar 999 ").Equals("MC#M-BAR_999"))
         );
 
     Console.WriteLine(
         "An encoded license plate can be decoded to display it properly: {0}",
-        FormatResult(DecodeLicensePlate("CAR#FOO_666").Equals("FOO 666")
+        FormatTestResult(DecodeLicensePlate("CAR#FOO_666").Equals("FOO 666")
         && DecodeLicensePlate("MC#BAZ_M-222").Equals("BAZ M-222"))
         );
 
     testSpaceId = 2;
     Console.WriteLine(
         "You can check if a parking space is empty: {0}",
-        FormatResult(ParkingSpaceIsEmpty(testSpaceId, testGarage) == true)
+        FormatTestResult(ParkingSpaceIsEmpty(testSpaceId, testGarage) == true)
         );
 
     testSpaceId = 1;
     AssignParkingSpaceToVehicle(testSpaceId, testCarLicensePlate, testGarage);
     Console.WriteLine(
         "You can check if a parking space is NOT empty: {0}",
-        FormatResult(ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true)
+        FormatTestResult(ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true)
         );
 
     testSpaceId = 2;
     Console.WriteLine(
         "You can assign an empty space to a vehicle: {0}",
-        FormatResult(AssignParkingSpaceToVehicle(testSpaceId, testCarLicensePlate, testGarage) == true)
+        FormatTestResult(AssignParkingSpaceToVehicle(testSpaceId, testCarLicensePlate, testGarage) == true)
         );
 
     testSpaceId = 2;
     AssignParkingSpaceToVehicle(testSpaceId, testCarLicensePlate, testGarage);
     Console.WriteLine(
         "After assigning an empty space to a vehicle, the space is not empty: {0}",
-        FormatResult(ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true)
+        FormatTestResult(ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true)
         );
+    Console.WriteLine(
+        "After clearing a non-empty space, the space is empty again: {0}",
+        FormatTestResult(
+            ParkingSpaceIsNotEmpty(testSpaceId, testGarage) == true
+            && ClearParkingSpace(testSpaceId, testGarage) == true
+            && ParkingSpaceIsEmpty(testSpaceId, testGarage) == true
+        ));
 
 
 
-    static bool FormatResult(bool result)
-    {
-        if (result == true)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-        }
-
-        return result;
-    }
 
     Console.ResetColor();
+}
+
+static bool ClearParkingSpace(int testSpaceId, string[] testGarage)
+{
+    int index = testSpaceId - 1;
+    testGarage[index] = "";
+    return ParkingSpaceIsEmpty(testSpaceId, testGarage);
 }
 
 /*****************************************************************************/
@@ -117,4 +118,20 @@ static string EncodeMotorcycleLicensePlate(string licensePlate)
 static string EncodeLicensePlate(string licensePlate, string vehicleType)
 {
     return $"{vehicleType}#{licensePlate.Trim().Replace(' ', '_').ToUpper()}";
+}
+
+/*****************************************************************************/
+
+static bool FormatTestResult(bool result)
+{
+    if (result == true)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+    }
+
+    return result;
 }
